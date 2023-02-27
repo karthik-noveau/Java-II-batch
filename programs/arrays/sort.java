db2b14fde1bf4e7f6a080aceb58e9ba4a91484ef
Karthik <karthik5112001@gmail.com>
2023-02-27 21:03:29 +0530

    Commit

--- programs/arrays/sort.java
+++ programs/arrays/sort.java
@@ -6,26 +6,29 @@ public class sort{
     int[] numbers = new int[]{5,8,6,4,7}; // 4, 5, 6, 7, 8
     int len = numbers.length;
 
-    for(int i=0; i<len; i++){    // 0
-
-         for(int j=i+1; j<len; j++){   // 3
-             if(numbers[i]>numbers[j]) // 5>4
-             { 
-               int temp = numbers[i];  //  temp = 5
-               numbers[i] = numbers[j];  // 5=4
-               numbers[j] = temp;  // 4=5
-
+    // for sorting
+    for(int i=0; i<len; i++){      
+                
+         for(int j=i+1; j<len; j++){    
+                       
+             if(numbers[i]>numbers[j]){   
+               int temp = numbers[i];   
+               numbers[i] = numbers[j];   
+               numbers[j] = temp; 
              } 
+
          }
 
     }
 
+    // print the sorted array values
     for(int i=0; i<len; i++){
       System.out.print(numbers[i] +" ");
     }
 
     System.out.println("");
 
+   // print 2nd min and max value from the array
     System.out.println("second minimum value is : " + numbers[1]);
     System.out.println("second maximum value is : " + numbers[len-2]);  //5-2
 
